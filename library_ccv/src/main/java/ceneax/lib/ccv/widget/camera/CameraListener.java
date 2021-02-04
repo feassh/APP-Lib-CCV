597fadbcb8639e75bd0ec33469e10d12e9b26616
ceneax <lxiian@qq.com>
2021-02-04 12:19:29 +0800

    update more

--- library_ccv/src/main/java/ceneax/lib/ccv/widget/camera/CameraListener.java
+++ library_ccv/src/main/java/ceneax/lib/ccv/widget/camera/CameraListener.java
@@ -12,7 +12,7 @@ public interface CameraListener {
      * @param displayOrientation 相机预览旋转角度
      * @param isMirror 是否镜像显示
      */
-    default void onCameraOpened(Camera camera, int cameraId, int displayOrientation, boolean isMirror) {}
+    void onCameraOpened(Camera camera, int cameraId, int displayOrientation, boolean isMirror);
 
     /**
      * 预览数据回调
@@ -24,19 +24,19 @@ public interface CameraListener {
     /**
      * 当相机关闭时执行
      */
-    default void onCameraClosed() {}
+    void onCameraClosed();
 
     /**
      * 当出现异常时执行
      * @param e 相机相关异常
      */
-    default void onCameraError(Exception e) {}
+    void onCameraError(Exception e);
 
     /**
      * 属性变化时调用
      * @param cameraID  相机ID
      * @param displayOrientation    相机旋转方向
      */
-    default void onCameraConfigurationChanged(int cameraID, int displayOrientation) {}
+    void onCameraConfigurationChanged(int cameraID, int displayOrientation);
 
 }
